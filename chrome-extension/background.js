8d98b0567435fd3b27a6f94e0d1d54538130f3ec
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-05-11 18:59:51 +0900

    refactor: " -> '

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,12 +1,12 @@
 
-const ContextMenuId = "a";
+const ContextMenuId = 'a';
 
 const createContextMenu = () => {
 	chrome.contextMenus.create({
-		title: "ページをメモ（Googleカレンダーに追加）",
+		title: 'ページをメモ（Googleカレンダーに追加）',
 		contexts: [
-			"page",
-			"selection"
+			'page',
+			'selection'
 		],
 		id: ContextMenuId
 	});
@@ -18,18 +18,18 @@ chrome.runtime.onStartup.addListener(createContextMenu);
 chrome.contextMenus.onClicked.addListener((info, tab) => {
 	if (info.menuItemId === ContextMenuId) {
 		const queryObject = {
-			action: "TEMPLATE",
+			action: 'TEMPLATE',
 			text: tab.title,
 			details: tab.url
 		};
 		if (info.selectionText) {
-			queryObject.details += "\n\n" + info.selectionText;
+			queryObject.details += '\n\n' + info.selectionText;
 		}
 
 		const querys = Object.entries(queryObject).map(([key, value]) => {
 			return `${key}=${encodeURIComponent(value)}`;
 		});
-		const calendarCreateUrl = "https://www.google.com/calendar/render?" + querys.join("&");
+		const calendarCreateUrl = 'https://www.google.com/calendar/render?' + querys.join('&');
 
 		const createProperties = {
 			url: calendarCreateUrl
